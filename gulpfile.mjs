b5fb8fb8fa91ce0044e044fcb671e3f0dbb10f99
the3rdchild <ytcrash555@gmail.com>
2025-08-07 06:41:06 +0000

    Update gitignore

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -3,6 +3,6 @@ import open from 'gulp-open';
 
 // Task to open the browser
 gulp.task('open-app', function(){
-  gulp.src('pages/dashboard.html')
+  gulp.src('pages/index.html')
   .pipe(open());
 });
