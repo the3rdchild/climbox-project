05041ee7e894485116fc5ef52e889adfa9780a9c
the3rdchild <ytcrash555@gmail.com>
2025-08-19 10:58:51 +0000

    Update user-related system (no firestore yet)

--- public/services/auth-template.js
+++
@@ -1,7 +0,0 @@
-// /services/auth.js
-import { getAuth, signInWithPhoneNumber } from "firebase/auth";
-import { app } from "./firestore"; // reuse initialized app
-
-const auth = getAuth(app);
-
-export { auth, signInWithPhoneNumber };
